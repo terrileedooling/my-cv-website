6b190592da3b13ee3a7607bcb2667702d3e3995a
Terri-Lee-Dooling-FutureRent <terri-lee@futurerent.co.za>
2025-04-21 00:00:12 +0200

    Updates

---
+++ precache-manifest.fce3d6cfeca74812c9358a92704f8bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c6aa93664766d806597dc0b0c435dc",
+    "url": "/my-cv-website/index.html"
+  },
+  {
+    "revision": "e4a5c3b69645f0e84f62",
+    "url": "/my-cv-website/static/css/main.4ce57e64.chunk.css"
+  },
+  {
+    "revision": "d6a18dbf49ba2be859d3",
+    "url": "/my-cv-website/static/js/2.15226794.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/my-cv-website/static/js/2.15226794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9e0c4b2f9dfa869228",
+    "url": "/my-cv-website/static/js/3.a2d127c1.chunk.js"
+  },
+  {
+    "revision": "e4a5c3b69645f0e84f62",
+    "url": "/my-cv-website/static/js/main.4d5e7ea0.chunk.js"
+  },
+  {
+    "revision": "5c198f9e752a3620aa80",
+    "url": "/my-cv-website/static/js/runtime-main.a67d3cc2.js"
+  }
+]);
