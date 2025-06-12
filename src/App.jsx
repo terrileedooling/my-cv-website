3ed9e1b1fbdf4ae935c03fe2e573f87beb029980
Terri-Lee-Dooling-FutureRent <terri-lee@futurerent.co.za>
2025-06-12 15:20:27 +0200

    fixed styling contact

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,6 @@ function App() {
       <About />
       <Skills />
       <Tabs />
-      <ContactForm />
-
       <section className="contact animate-bounce-scale">
         <h2> < FaPhone /> Contact</h2>
         <p>Email: terrileedooling@gmail.com</p>
@@ -24,15 +22,7 @@ function App() {
           <a href="https://github.com/terrileedooling">GitHub</a> | <a href="https://www.linkedin.com/in/terri-lee-d-0b6700236/">LinkedIn</a>
         </p>
       </section>
-
-      {/* <section className="contact-form animate-bounce-scale">
-        <form className="contact-form">
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="Email" required />
-          <textarea placeholder="Your message..." required />
-          <button type="submit">Send</button>
-        </form>
-      </section> */}
+      <ContactForm />
     </div>
   );
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,44 +2,55 @@ import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 
 function ContactForm() {
-    const [state, handleSubmit] = useForm(import.meta.env.FORMSPREE_FORM_ID);
+    const [state, handleSubmit] = useForm(import.meta.env.VITE_FORMSPREE_FORM_ID);
 
     if (state.succeeded) {
         return <p>Thank you for contacting me.</p>
     }
 
     return (
-        <form action={import.meta.env.FORMSPREE_URL} method="POST">
-            <label htmlFor="email" className="font-semibold">
-                Email Address
-            </label>
-            <input
-                id="email"
-                type="email"
-                name="email"
-                required
-                className="border border-gray-300 p-2rounded"
-            />
-            <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <form action={import.meta.env.VITE_FORMSPREE_URL} method="POST" className="contact-form animate-bounce-scale">
+            <div className="form-input">
+                <label htmlFor="name">
+                    Name
+                </label>
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    required
+                />
+                <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-            <label htmlFor="message" className="font-semibold">
-                Message
-            </label>
-            <textarea
-                id="message"
-                name="message"
-                rows="6"
-                required
-                className="border border-gray-300 p-2rounded"
-            />
-            <ValidationError prefix="Message" field="message" erros={state.error} />
+                <label htmlFor="email">
+                    Email Address
+                </label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    required
+                />
+                <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+                <label htmlFor="message">
+                    Message
+                </label>
+                <textarea
+                    id="message"
+                    name="message"
+                    rows="6"
+                    required
+                />
+                <ValidationError prefix="Message" field="message" erros={state.error} />
+
+            </div>
 
             <button
                 type="submit"
                 disabled={state.submitting}
-                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
             >
-                {state.submitting ? 'Sending...' : 'Send Message'}
+                {state.submitting ? 'Sending...' : 'Send'}
             </button>
         </form>
     )
